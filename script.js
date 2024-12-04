747296634132fd1d37284d758e06345c438f79c4
JamesDrysdale <jamesdrysdale84@gmail.com>
2024-12-04 21:20:29 +0000

    refactor: encapsulate JSON fetching logic in a dedicated function

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 // Fetch and log JSON data
-fetch('./artist-dates.json')
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data); // Log the array to confirm access
-  })
-  .catch((error) => {
-    console.error('Error fetching JSON:', error);
+const dataFilepath = './artist-dates.json'
+
+function fetchData(dataFilepath) {
+    console.log(dataFilepath);
+    return fetch(dataFilepath)
+        .then((response) => {
+            if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data); // Log the array to confirm access
+        })
+        .catch((error) => {
+            console.error('Error fetching JSON:', error);
   });
+}
+
+fetchData(dataFilepath);
