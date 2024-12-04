6ef933c440eeb7092765f793596d066ecafdb769
JamesDrysdale <jamesdrysdale84@gmail.com>
2024-12-04 22:05:20 +0000

    Logic for generating a single random idea

--- script.js
+++ script.js
@@ -1,20 +1,39 @@
 // Fetch and log JSON data
-const dataFilepath = './artist-dates.json'
+const dataFilepath = './artist-dates.json';
 
 function fetchData(dataFilepath) {
     return fetch(dataFilepath)
         .then((response) => {
             if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
+                throw new Error(`HTTP error! status: ${response.status}`);
             }
             return response.json();
         })
-        .then((data) => {
-            console.log(data); // Log the array to confirm access
-        })
         .catch((error) => {
             console.error('Error fetching JSON:', error);
-  });
+            throw error;
+        });
 }
 
-fetchData(dataFilepath);
+// Randonly Select an Idea
+
+function generateRandomIdea(ideas) {
+    if (!ideas || ideas.length === 0) return "There are no ideas available right now, please try again later.";
+    const randomIndex = Math.floor(Math.random() * ideas.length);
+    return ideas[randomIndex].description;
+}
+
+// Fetch and Generate Idea
+
+async function handleGenerateIdea() {
+    try {
+        const ideas = await fetchData(dataFilepath);
+        let idea = generateRandomIdea(ideas);
+        console.log('Generated Idea: ', idea);
+    } catch (error) {
+        console.error('Error generating idea: ', error);
+    }
+}
+
+// Call function for testing
+handleGenerateIdea();
